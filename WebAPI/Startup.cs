673bec9e72bebf5ad6f5f8ef3bcd482ceb3971f6
minh ha <haquangminh98@gmail.com>
2021-05-27 09:20:49 +0700

    add ioc

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.AppStart;
 using static DVG.CK.OMSApi.Filter.CustomizeAuthorizeAttribute;
 
 namespace WebAPI
@@ -26,6 +27,7 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            IOC.RegisterTypes(services);
             services.AddControllers();
             services.AddMvc(options =>
             {
