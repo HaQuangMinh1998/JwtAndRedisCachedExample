0ea9f09416ca3205a5a475aa82e092c5a79784b8
minh ha <haquangminh98@gmail.com>
2021-05-27 15:09:09 +0700

    update

--- Business/User/IUser.cs
+++ Business/User/IUser.cs
@@ -11,7 +11,7 @@ namespace Business.User
         ResponseData Logout();
         bool ChecksumJWTOnCache(string keyCached);
         bool CheckExitstJWTTokenOnCache(string keyCached, string token);
-        bool SaveJWTTokenOnCache(string tokenName, string key, string token);
+        bool SaveTokenOnCache(string tokenName, string key, string token);
         bool CheckLockRefreshTokenOnCache(string keyCached);
         bool SetLockRefreshTokenOnCache(string checksumKey);
         bool DeleteJWTTokenOnCache(string tokenName, string key);
